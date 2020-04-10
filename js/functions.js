032b3b786b55a212d02381bd7bab5c5ee8975859
nicolasgomiero <58782233+nicolasgomiero@users.noreply.github.com>
2020-04-10 11:03:04 +0200

    MaJ Competences

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+var experiences = document.querySelector("#ancreExp");
+var formation = document.querySelector("#ancreFormation");
+var interets = document.querySelector("#ancreInteret");
+var button = document.querySelectorAll(".btn");
+
+function chevron(){
+    //button. = "true";
+}
