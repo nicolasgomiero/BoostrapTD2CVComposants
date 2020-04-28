e46dcdbfd264c05ce517aa320c1180ab16a1d9ca
nicolasgomiero <58782233+nicolasgomiero@users.noreply.github.com>
2020-04-28 11:08:37 +0200

    Update laravel
    25% laravel et row = m0

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,17 @@
 var experiences = document.querySelector("#ancreExp");
 var formation = document.querySelector("#ancreFormation");
 var interets = document.querySelector("#ancreInteret");
-var button = document.querySelectorAll(".btn");
+var button = document.querySelectorAll("button");
+let img = document.querySelector("img");
 
 function chevron(){
-    //button. = "true";
+    if (img.src === "images/chevron-up-solid.svg")
+    {
+        img.src = "images/chevron-down-solid.svg";
+    }else img.src = "images/chevron-up-solid.svg";
+}
+
+
+for(var i = 0;i < button.length-1;i++) {
+    button[i].addEventListener("click", chevron);
 }
